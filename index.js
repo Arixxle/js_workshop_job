f619fa56a82103ed95d53acea076f5d45d4d4ea4
Joseph Hung <arixxle@gmail.com>
2020-04-28 14:19:57 +0800

    feature task 2

--- index.js
+++ index.js
@@ -2,7 +2,18 @@
 
 let ham = document.querySelector('#navbar-burger')
 var menu = document.querySelector('#navbar-menu')
+let url = 'https://still-spire-37210.herokuapp.com/positions.json'
+const JOBS = []
 
+/* feature/tack_2 */
+fetch(url).then(function(response) {
+  return response.json();
+  // console.log(response.json());
+}).then(function (value) { JOBS.push(value)})
+console.log(JOBS);
+
+/* feature/task_1 */
 ham.addEventListener('click', function () {
   menu.classList.toggle('is-active');
 })
+
